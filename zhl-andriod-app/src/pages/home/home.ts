356fd0d187a3870f6ed112dc94d860c9d66071d2
Leochens <18332518328@163.com>
2018-04-21 14:57:41 +0800

    update 开始学习angular2

--- zhl-andriod-app/src/pages/home/home.ts
+++ zhl-andriod-app/src/pages/home/home.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
+/*装饰器*/
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html'//模板
 })
+/*组件类*/
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
@@ -12,3 +15,5 @@ export class HomePage {
   }
 
 }
+
+
